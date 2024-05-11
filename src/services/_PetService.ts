01170e19149f1f34878c99383a37477246aa8263
ArielZarate <arieltecnico@gmail.com>
2024-05-11 11:29:06 -0300

    CONFIGURANDO CORS V1.7

--- src/services/_PetService.ts
+++ src/services/_PetService.ts
@@ -1,8 +1,8 @@
-import URL_BASE from "@/utils/config";
+import NEXT_PUBLIC_URL_BASE from "@/utils/config";
 
 //podria usar axios tambien en vez de fetch y configurar las urlbase
 export const fetchBackendPets = async () => {
-  const res = await fetch(`${URL_BASE}/api/routes/pets`);
+  const res = await fetch(`${NEXT_PUBLIC_URL_BASE}/api/routes/pets`);
   let data = await res.json();
 
   return data;
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,9 +1,9 @@
 // config.js
 const { NEXT_PUBLIC_BASE_API_URL } = process.env;
 
-const URL_BASE =
+const NEXT_PUBLIC_URL_BASE =
   process.env.NODE_ENV === "production"
     ? `${NEXT_PUBLIC_BASE_API_URL}`
     : "http://localhost:3000";
 
-export default URL_BASE;
+export default NEXT_PUBLIC_URL_BASE;
