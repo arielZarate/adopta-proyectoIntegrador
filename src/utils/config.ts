87f5b9c920fa8f314a05adc7c9a1a07a96ccd0b4
ArielZarate <arieltecnico@gmail.com>
2024-05-11 11:22:47 -0300

    CONFIGURANDO CORS V1.7

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,7 +1,9 @@
 // config.js
+const { NEXT_PUBLIC_BASE_API_URL } = process.env;
+
 const URL_BASE =
   process.env.NODE_ENV === "production"
-    ? "https://adopta.vercel.app"
+    ? `${NEXT_PUBLIC_BASE_API_URL}`
     : "http://localhost:3000";
 
 export default URL_BASE;
