b09a5f966481bfd34d343a45149194f5fc63c415
ArielZarate <arieltecnico@gmail.com>
2024-05-13 13:55:02 -0300

    CONFIGURANDO CORS V1.8

--- src/services/_PetService.ts
+++ src/services/_PetService.ts
@@ -1,15 +1,17 @@
 import NEXT_PUBLIC_URL_BASE from "@/utils/config";
 
 export const fetchBackendPets = async () => {
+  /*
   if (!NEXT_PUBLIC_URL_BASE) {
     console.error(
       "La variable de entorno NEXT_PUBLIC_URL_BASE no está definida."
     );
     return null; // Otra opción podría ser lanzar un error aquí.
   }
+ */
 
   try {
-    const res = await fetch(`${NEXT_PUBLIC_URL_BASE}/api/routes/pets`);
+    const res = await fetch(`/api/routes/pets`);
     const data = await res.json();
     return data;
   } catch (error) {
